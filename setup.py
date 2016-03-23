b918d53e148b16b81238c675cbd1ea5f6853df32
Nishant Nikhil <nishantiam@gmail.com>
2016-03-24 00:18:31 +0530

    Changed Programming Language Classifier

--- setup.py
+++ setup.py
@@ -25,7 +25,10 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
